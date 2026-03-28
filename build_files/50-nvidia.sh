3ec2dc8b568c811012aa9bb6824e155b24a06c02
threadweaver <deep.cat0869@fastmail.com>
2026-03-28 15:51:36 -0700

    Added the nvidia build script

---
+++ build_files/50-nvidia.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+if [[ ! "${BUILD_FLAVOR}" =~ "nvidia" ]] ; then
+  exit 0
+fi
+
+# We just need to rebuild it since the base image already has these :)
+
+mkdir -p /var/log/akmods /var/tmp
+chmod 1777 /var/tmp
+KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
+akmods --force --kernels "${KERNEL_VERSION}" --kmod "nvidia"
+cat /var/cache/akmods/nvidia/*.failed.log || true
+stat "/usr/lib/modules/${KERNEL_VERSION}"/extra/nvidia/nvidia*.ko* # Ensuring kmods exist
