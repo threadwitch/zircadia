c7a044c25908dc4955dbc79e3e52d68e294e9fc2
threadweaver <deep.cat0869@fastmail.com>
2026-03-28 14:00:55 -0700

    Added the post-install script from bazzirco, and modified it for Zircadia

---
+++ build_files/99-final-hooks.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+sed -i -f - /usr/lib/os-release <<EOF
+s|^NAME=.*|NAME=\"Zircadia\"|
+s|^PRETTY_NAME=.*|PRETTY_NAME=\"Zircadia\"|
+EOF
+
+echo 'import "/usr/share/zirconium/just/67-gamerslop.just"' >> /usr/share/zirconium/just/00-start.just
+
+KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
+export DRACUT_NO_XATTR=1
+dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
+chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
