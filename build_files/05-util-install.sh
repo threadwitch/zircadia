26740ef6a4391fdf12f4a9956b0a312ada0d8422
threadweaver <deep.cat0869@fastmail.com>
2026-04-06 18:53:43 -0700

    Removed MEGAsync entirely for being too much trouble.

--- build_files/05-util-install.sh
+++ build_files/05-util-install.sh
@@ -2,12 +2,6 @@
 
 set -xeou pipefail
 
-# Installing MEGAsync at the base level just because I don't want to deal with distroboxes and I'm setting this up.
-# This doesn't work, it just segfaults on launch. wtf?
-# dnf -y --enablerepo=rpmfusion-nonfree --enablerepo=rpmfusion-free install --allowerasing megasync
-
-dnf -y install https://mega.nz/linux/repo/Fedora_43/x86_64/megasync-Fedora_43.x86_64.rpm
-
 dnf -y --enablerepo=terra install \
 	helium-browser-bin \
 	vesktop
