b01efc0659ce47cc5b5839fc5187ab72a7802e14
threadweaver <deep.cat0869@fastmail.com>
2026-04-19 12:57:47 -0700

    Saved gaming-post, but I don't know what, if any, changes there are, and I'm not using it anyways because I don't use Xbox One controllers.

--- build_files/09-gaming-post.sh
+++ build_files/09-gaming-post.sh
@@ -2,8 +2,6 @@
 
 set -xeuo pipefail
 
-cp -avf "/ctx/files"/. /
-
 KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
 
 dkms install -m "xone/$(basename "$(find /usr/src -iname "*xone*" -type d)" | cut -f2 -d-)" -k "${KERNEL_VERSION}"
